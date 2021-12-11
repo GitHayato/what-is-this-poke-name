6ca6e1ed4e75c6bdabfeb475c634f5346a8b0073
GitHayato <gabayt0_0@icloud.com>
2021-12-11 18:27:23 +0900

    Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,12 @@ import { Provider } from 'react-redux'
 import type { AppProps } from 'next/app'
 
 import { store } from '../app/store'
+import { Header } from '../components/Header'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Header />
       <Component {...pageProps} />
     </Provider>
   )
