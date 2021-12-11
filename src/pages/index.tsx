fa794edc1dd3876668463e077fd4f31f2449f800
GitHayato <gabayt0_0@icloud.com>
2021-12-11 16:12:00 +0900

    basic features

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,52 @@
-import axios from 'axios'
 import Image from 'next/image'
 import React from 'react'
-import { PokemonType } from '../types/pokemon/:id'
-import { POKE_API_URL } from '../utils/endpoints'
 
-const IndexPage = ({pokemonData}: {pokemonData: PokemonType}) => {
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { useGetPokemonByIdQuery } from '../app/queries/pokeApi'
+import { stockIdSlice, selectStockIds } from '../app/slice/stockIdSlice'
+import { Loading } from '../components/Loading'
+import { useRandom } from '../hooks/useRandom'
+
+const IndexPage = () => {
+  const [queryId, setQueryId] = React.useState('')
+
+  const stockIds = useAppSelector(selectStockIds)
+  const { pokemonId } = stockIdSlice.actions
+  const dispatch = useAppDispatch()
+
+  const dispatchPokemonId = React.useCallback(() => {
+    const { randomNumber } = useRandom()
+    dispatch(pokemonId(randomNumber))
+    setQueryId(randomNumber)
+  }, [])
+
+  React.useEffect(() => {
+    dispatchPokemonId()
+  }, [])
+
+  const { data: pokemonData, refetch: refetchPokemonData, isFetching } = useGetPokemonByIdQuery(queryId)
+
+  const handleClickNextPokemon = React.useCallback(() => {
+    dispatchPokemonId()
+    refetchPokemonData()
+  }, [dispatchPokemonId, refetchPokemonData])
+
   return (
     <div>
-      <h1>{pokemonData.name}</h1>
-      <Image src={pokemonData.sprites.front_default} width={200} height={200} />
+      {isFetching ? (
+        <Loading />
+      ) : (
+        <>
+          <h1>{pokemonData.name}</h1>
+          <Image src={pokemonData.sprites.front_default} width={200} height={200} />
+          <button onClick={handleClickNextPokemon}>Next Pokemon</button>
+          {stockIds.map((id) => (
+            <p key={id} onClick={() => setQueryId(id)}>{id}</p>
+          ))}
+        </>
+      )}
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const fetchData = await axios.get<PokemonType>(`${POKE_API_URL}/809`)
-  const pokemonData = fetchData.data
-
-  return {
-    props: {
-      pokemonData,
-    },
-  }
 }
 
 export default IndexPage
