aa9a7eecbb3e59abe106048da355f1d7f0a75d79
GitHayato <gabayt0_0@icloud.com>
2021-12-11 12:18:14 +0900

    fetchData

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
-import type { NextPage } from 'next'
+import axios from 'axios'
+import Image from 'next/image'
+import React from 'react'
+import { PokemonType } from '../types/pokemon/:id'
+import { POKE_API_URL } from '../utils/endpoints'
 
-const IndexPage = () => {
+const IndexPage = ({pokemonData}: {pokemonData: PokemonType}) => {
   return (
     <div>
-      Hello, PokeWorld!
+      <h1>{pokemonData.name}</h1>
+      <Image src={pokemonData.sprites.front_default} width={200} height={200} />
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const fetchData = await axios.get<PokemonType>(`${POKE_API_URL}/809`)
+  const pokemonData = fetchData.data
+
+  return {
+    props: {
+      pokemonData,
+    },
+  }
 }
 
 export default IndexPage
